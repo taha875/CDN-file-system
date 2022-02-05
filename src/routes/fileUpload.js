f17b16871cbbf85167edb5053b9813e32a5971bc
umer <ssyedumer@gmail.com>
2022-02-05 03:57:58 -0800

    structure straightend

--- src/routes/fileUpload.js
+++ src/routes/fileUpload.js
@@ -12,58 +12,58 @@ const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, 'upload/')
     },
-    filename:async function (req, file, cb) {
+    filename: async function (req, file, cb) {
         cb(null, Date.now() + slugify(file.originalname))
-        
-  	},
-    limits:{fieldSize:10 * 1024 *1024}
+
+    },
+    limits: { fieldSize: 10 * 1024 * 1024 }
 })
 const upload = multer({ storage: storage })
 router.route('/document')
-.post(upload.single('document'), 
+    .post(upload.single('document'),
 
-(req, res, next) => { 
-try {
-    if(!req || !req.file){
-        throw{
-            code:400,
-            message:"please provide a file"
-        }
-    }
-    return res.status(200).send({success:true, response:req.file})
-    
-} catch (error) {
-    let code = error.code || 500
-    let message = error.message || JSON.stringify(error)
-    return res.status(code).send({error:true, message:message})
-}
+        (req, res, next) => {
+            try {
+                if (!req || !req.file) {
+                    throw {
+                        code: 400,
+                        message: "please provide a file"
+                    }
+                }
+                return res.status(200).send({ success: true, response: req.file })
 
-})
+            } catch (error) {
+                let code = error.code || 500
+                let message = error.message || JSON.stringify(error)
+                return res.status(code).send({ error: true, message: message })
+            }
+
+        })
 
 
-function convertUnixTime(time){
+function convertUnixTime(time) {
 
     let unixtimestamp = time
 
-    let months_arr = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
-   
-    let date = new Date(unixtimestamp*1000);
- 
+    let months_arr = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+    let date = new Date(unixtimestamp * 1000);
+
     let year = date.getFullYear();
-   
+
     let month = months_arr[date.getMonth()];
-  
+
     let day = date.getDate();
-   
+
     let hours = date.getHours();
-   
-    let minutes =  date.getMinutes();
-   
-    let seconds =  date.getSeconds();
-   
+
+    let minutes = date.getMinutes();
+
+    let seconds = date.getSeconds();
+
     let readableTime = `${day}-${month}-${year}T${hours}:${minutes}:${seconds}`;
     return readableTime
-    
-    
-   }
+
+
+}
 module.exports = router 
